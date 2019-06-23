f38954639c2bc24a2f8828756fd267065bcb0bfa
Axel Resnik <axelresnik@gmail.com>
2019-06-23 16:04:35 +0300

    models validations

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,4 +1,15 @@
 class Island < ApplicationRecord
+  DIFFICULTY = %w[easy medium hard very/ hard]
+
   belongs_to :user
-  belongs_to :review
+  has_many :reviews
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :activities, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
+  validates :difficulty, presence: true, inclusion: { in: DIFFICULTY }
+  validates :photo, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  has_many :bookings
+  has_many :islands
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
