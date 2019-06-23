004faa1edacc6a7105dae8091833f3cbec3d9d1c
Axel Resnik <axelresnik@gmail.com>
2019-06-23 20:07:07 +0300

    started controller for booking and sing in with pictures

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,29 @@
 class BookingsController < ApplicationController
+  before_action :find_island, only: [:new, :create]
+
   def new
+    @booking = Booking.new
   end
 
   def create
+    raise
+    @booking = Booking.new(booking_params)
+    @booking.island = @island
+    @booking.save
   end
 
   def destroy
   end
 
   def dashboard
+  end
 
+  private
+  def find_island
+    @island = Island.find(params[:island_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :user_id)
   end
 end
