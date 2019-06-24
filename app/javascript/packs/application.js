d2beeab30e24df5d7ace51cabf18126e556a1cc5
Axel Resnik <axelresnik@gmail.com>
2019-06-24 17:36:25 +0300

    solving

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import { tooltip } from '../components/tooltip'
 import "../plugins/flatpickr"
+import { imagescroll } from '../components/imagescroll'
 
 
 tooltip()
+imagescroll()
