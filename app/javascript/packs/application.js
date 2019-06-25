96c216c6fe9785163023156632edbf461e3fa27e
Ben Attali <ben.attali8@gmail.com>
2019-06-25 12:21:23 +0300

    Frontend for index and new views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@ import { tooltip } from '../components/tooltip'
 import "../plugins/flatpickr"
 import { imagescroll } from '../components/imagescroll'
 
+import { initAutocomplete } from '../plugins/init_autocomplete'
 
-imagescroll()
-tooltip()
-imagescroll()
+tooltip();
+imagescroll();
+initAutocomplete();
