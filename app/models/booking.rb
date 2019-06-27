c95f731fa0c521ca41fc615bb888df58f55b361a
Axel Resnik <axelresnik@gmail.com>
2019-06-27 13:13:20 +0300

    flatpickr

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :island
   belongs_to :user
   belongs_to :transport, optional: true
+  validates_presence_of :start_date, :end_date
+  # validate :end_date_is_after_start_date
 end
